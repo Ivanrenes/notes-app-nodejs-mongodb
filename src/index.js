28862d1a4da650e6a37ccbb9f8e47738b89f3b24
ivancode <ivanrenescorcia@gmail.com>
2021-01-07 00:25:56 -0500

    passport, User model added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const expresshbs = require('express-handlebars')
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
 
 
 //Initializations
 const app = express();
 require('./database');
+require('./config/passport')
 
 //Settings
 app.set('port', process.env.PORT || 3000);
@@ -17,7 +19,11 @@ app.engine('.hbs', expresshbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'),'layouts'),
     partialsDir: path.join(app.get('views'),'partials'),
-    extname: '.hbs'
+    extname: '.hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true,
+      },
 }));
 
 app.set('view engine', '.hbs');
@@ -30,12 +36,16 @@ app.use(session({
     resave: 'true',
     saveUninitialized: 'true'
 }))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
-
 //Globals Variables
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    res.locals.username = req.user;
 
     next();
 })
